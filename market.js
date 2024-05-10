4ee9bbbed13e63d9effff6c93cae95305aabb512
Nikasms <23b_sns@liceum.ztu.edu.ua>
2024-05-11 00:16:57 +0300

    123-124

--- market.js
+++ market.js
@@ -1,15 +1,35 @@
-
 //перевірка підключеного файлу скриптів
-console.log('перевірка підключеного файлу скрипту market.js')
+// console.log('перевірка підключеного файлу скрипту market.js')
+
+//визначення масиву товарів
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N'
+]
 
 //отримання елементу з ідентифікатором items
 let itemsDiv = document.getElementById("items")
 
-//вивід знайдкного елементу
-console.log(itemsDiv)
-
 //перевірка існування знайденого блоку
 if (itemsDiv) {
+    //створення блоків по кількості елементів 
+    itemsArray.forEach((item) =>{
+        // console.log(item)
+        // itemsDiv.innerText += item
+        //спеціальний апостроф - англійська розкладка - біля кнопки 1 
+        itemsDiv.innerHTML += 
+        `
+        <div class = "item">
+        //  <h2>Товар№${index}</h2>
+        //  <p>${item}</>
+        </div>
+        `
+})
+
+
+    
     //вивід знайденого елементу
     // console.log(itemsDiv)
 
@@ -35,15 +55,33 @@ if (itemsDiv) {
     // }
 
 } else {
-    //вивід знайденого елементу
+    //вивід повідомлення про не знайдений блок
     console.log('блок товарів не знайдено')
 }
-let itemsArray = ['','','','']
+
+
+
+// //виведення елементів відсортованого масиву
+// itemsArray.sort().forEach((item, index) =>{
+//     console.log(index + '-й елемент', item)
+// })
+
+
+//сортування масиву
+// itemsArray = itemsArray.sort()
 
 //виведення в консоль масиву
-console.log(itemsArray)
-for(let i = 0; i < 100; i++) {
-    //     itemsDiv.innerHTML += '<div class="item"></div>'
-    //     console.log('item')
-    }
+    console.log(itemsArray)
+
+//вииведення в консоль номерів та значень елементів масиву
+// for(let i = 0; i < itemsArray.length; i++) {
+//          console.log( i + '-й елемент:', itemsArray[i])
+//      }
+
+//виведення в консоль масиву
+// console.log(itemsArray)
+// for(let i = 0; i < 100; i++) 
+//     itemsDiv.innerHTML += '<div class="item"></div>'
+ //     console.log('item')
+//     }
 
